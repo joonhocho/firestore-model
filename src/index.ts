94894e368ccb4ac845e01b8ea73086a55ca09b76
Joon Ho Cho <joonho1101@gmail.com>
2018-12-17 22:39:58 -0800

    export *

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from './firebase';
+export * from './firestore';
+export * from './util';
 import * as firebase from './firebase';
 import * as firestore from './firestore';
 import * as util from './util';
